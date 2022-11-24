f57d722a223084f6803b3f836c64c37eb24d30ac
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2022-11-24 13:06:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.afkplob.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const CarOptionCollection = client.db('CarsPortal').collection('CarsCollection')
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err))
+
+app.get('/', async (req, res) => {
+    res.send('Cars Portal server is running ')
+})
+
+app.listen(port, () => {
+    console.log(`Cars Portal running on ${port}`)
+})
+
+//CarsPortal
+
+//CarsCollection
